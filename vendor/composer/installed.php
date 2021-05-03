bb4232dc1388617a61469663c6e089b7c7e3110d
Jarrad Salmon <jbardnz@gmail.com>
2021-05-03 13:37:15 +1200

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '3.0.165',
+    'version' => '3.0.165.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'processwire/processwire',
+  ),
+  'versions' => 
+  array (
+    'processwire/processwire' => 
+    array (
+      'pretty_version' => '3.0.165',
+      'version' => '3.0.165.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
